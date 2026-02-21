c75a02ad1120961b4bfa2de793d4b67fc7feaaa2
DozchaFranzo <brianjonesmanaois60@gmail.com>
2026-02-21 15:59:27 +0800

    script.js

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ function toggleDarkMode() {
   }
 }
 
-// Apply saved theme on load
+// =========================
+// PAGE LOAD EVENT
+// =========================
+
 window.addEventListener("DOMContentLoaded", () => {
   const savedTheme = localStorage.getItem("theme");
 
@@ -45,21 +48,24 @@ function revealOnScroll() {
 }
 
 // =========================
-// MOTIVATION
+// MOTIVATION BUTTON
 // =========================
 
 function showMotivation() {
   const messages = [
     "Believe in yourself, even when no one else does -Lagahan",
-    "Your best is more than just being Enough -De lemos",
+    "Your best is more than just being Enough -De Lemos",
     "It's only that you've lost everything, you're free to do everything -Manaois",
     "Believe you can and you're halfway there -Idloy",
-    "Give it your all to receive everything.-Gary the Chink"
-    "Pagtalo sa pustahan, Riot na -Austria na may pimple"
-    "Victory justifies any means -San miguel Extra Strong"
+    "Give it your all to receive everything -Gary the Chink",
+    "Pagtalo sa pustahan, Riot na -Austria na may pimple",
+    "Victory justifies any means -San Miguel Extra Strong"
   ];
 
   const randomIndex = Math.floor(Math.random() * messages.length);
-  document.getElementById("motivationText").innerText =
-    messages[randomIndex];
+
+  const target = document.getElementById("motivationText");
+  if (target) {
+    target.innerText = messages[randomIndex];
+  }
 }
