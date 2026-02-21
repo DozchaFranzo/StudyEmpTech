0cc4b03715c48eb6c046bdf728c99889f7b8cc4b
DozchaFranzo <brianjonesmanaois60@gmail.com>
2026-02-21 20:12:33 +0800

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// DARK MODE
-
 function toggleDarkMode(){
-
 document.body.classList.toggle("dark");
 
 if(document.body.classList.contains("dark")){
@@ -9,58 +6,25 @@ localStorage.setItem("theme","dark");
 }else{
 localStorage.setItem("theme","light");
 }
-
 }
 
-// LOAD
-
 window.addEventListener("DOMContentLoaded",()=>{
-
 if(localStorage.getItem("theme")==="dark"){
 document.body.classList.add("dark");
 }
-
-revealOnScroll();
-
 });
-
-// SCROLL ANIMATION
-
-function revealOnScroll(){
-
-const elements=document.querySelectorAll(".reveal");
-
-elements.forEach(el=>{
-el.classList.add("animate");
-});
-
-const observer=new IntersectionObserver(entries=>{
-
-entries.forEach(entry=>{
-if(entry.isIntersecting){
-entry.target.classList.add("active");
-}
-});
-
-},{threshold:0.2});
-
-elements.forEach(el=>observer.observe(el));
-
-}
-
-// MOTIVATION
 
 function showMotivation(){
 
 const messages=[
-"Believe in yourself, even when no one else does",
-"Your best is more than being enough",
-"You are stronger than you think",
-"Progress matters more than perfection",
-"Discipline builds future success"
+"Believe in yourself, even when no one else does.",
+"Your best is more than enough.",
+"You are stronger than you think.",
+"Progress matters more than perfection.",
+"Discipline builds future success."
 ];
 
-document.getElementById("motivationText").innerText=
+document.getElementById("motivationText").innerText =
 messages[Math.floor(Math.random()*messages.length)];
 
 }
